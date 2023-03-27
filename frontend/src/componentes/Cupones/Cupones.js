62a3d8e257630cb6e6cc2acafd62d252e5c9a48a
Breayanxd <breayanmt10@hotmail.com>
2023-03-27 07:29:51 -0600

    Cupones v3

--- frontend/src/componentes/Cupones/Cupones.js
+++ frontend/src/componentes/Cupones/Cupones.js
@@ -6,28 +6,31 @@ import marioImage from "./imagenesCupones/Mario.gif"
 import teamImage from "./imagenesCupones/Team.gif"
 import { useNavigate } from "react-router-dom";
 import * as CuponesServer from './CuponesServer';
+import Popup from "./Popup/Popup";
 
 const Cupones=()=>{
-    // const history = useNavigate();
-    // const [cupones, setCupones] = useState([]);
-    // const listaCupones = async() =>{
-    //     try {
-    //         const response = await CuponesServer.getCupones();
-    //         const data = await response.json();
-    //         setCupones(data.cupones); 
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // }
-    // useEffect(()=>{
-    //     listaCupones();
-    // },[]);
+    //const history = useNavigate();
+    const [cupones, setCupones] = useState([]);
+    const [estadoModal, setEstadoModal] = useState(true)
+    const listaCupones = async() =>{
+        try {
+            const response = await CuponesServer.getCupones();
+            const data = await response.json();
+            setCupones(data.Cupones); 
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    useEffect(()=>{
+        listaCupones();
+    },[]);
 
+    let imagenesCupones = [haloImage, kratosImage, marioImage, teamImage]
     let cupon1=[haloImage, "Cupón Bienvenida", "Obten un 15% de descuento en tu primera compra",1]
     let cupon2=[kratosImage, "Cupón De Envío Gratis", "Tu próximo envío a domicilio de un juego en formato físico es gratis",4]
     let cupon3=[marioImage, "Cupón 2x1", "En la compra de dos juegos menores a $100 mxn paga solo uno",3]
     let cupon4=[teamImage, "Cupón De Descuento", "Obtén un 5% de descuento en compras mayores a $1,500 mxn","ilimitado"]
-    let cupones = [cupon1, cupon2, cupon3, cupon4]
+    //let cupones = [cupon1, cupon2, cupon3, cupon4]
     return(
         <>
         <div id={style.cabecera}>
@@ -36,15 +39,16 @@ const Cupones=()=>{
         <div id={style.contenedorTarjetas}>
             {cupones.map((cupon)=>(
                 <div id={style.tarjeta} className="card">
-                    <img id={style.imgCard} src={cupon[0]} className="card-image-top" alt=""/>
+                    <img id={style.imgCard} src={cupon.imagen} className="card-image-top" alt=""/>
                     <div className="card-body" id={style.cuerpoTarjeta}>
-                        <h5>{cupon[1]}</h5>
-                        <p>{cupon[2]}</p>
-                        <p>usos: {cupon[3]}</p>
+                        <h5>{cupon.nombre}</h5>
+                        <p>{cupon.descripcion}</p>
                     </div>
                 </div>
             ))}
         </div>
+        <button className="btn-primary" onClick={()=>setEstadoModal(!estadoModal)}>Popup</button>
+        <Popup estado={estadoModal} cambiarEstado={setEstadoModal}/>  
         </>
     );
 };
