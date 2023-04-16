4cda4ea3052d264620569979b9cd940dba9c350c
MarcosDCP <zs20018147@estudiantes.uv.mx>
2023-04-15 21:50:36 -0600

    ayuda

--- backend/firebase/forms/entidades/VideojuegoF.py
+++ backend/firebase/forms/entidades/VideojuegoF.py
@@ -1,13 +1,151 @@
-from django import forms
+from django.apps import apps
+from django.shortcuts import render, redirect
+from django.http.response import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+from firebase.database.Firebase import Firebase
+from firebase.database.entidades.Videojuego import Videojuego
+import json
 
-class VideojuegoF(forms.Form):
-    _id = forms.IntegerField(required = True)
-    _nombre = forms.CharField(max_length = 30, required = True)
-    _descripcion = forms.CharField(max_length = 500, required = True)
-    _caratula = forms.ImageField(required = True)
-    _video = forms.URLField(required = True)
-    _precio = forms.FloatField(required = True)
-    _genero = forms.CharField(max_length = 250, required = True)
-    _plataforma = forms.CharField(max_length = 20, required = True)
-    _datosExtra = forms.CharField(max_length = 500, required = True)
-    _calificacion = forms.FloatField(required = True)
+db = Firebase()
+documento = "Videojuegos"
+
+class VideojuegoV(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, id = -1, ids = ""):
+        if db.conexionDB and request.method == "GET":
+            videojuegos = list()
+
+            if id > -1 and ids == "":
+                for key, value in db.getDocumento(documento).items():
+                    if value != None and str(value["id"]) == str(id):
+                        videojuegos.append({
+                            "id": value["id"],
+                            "nombre": value["nombre"],
+                            "descripcion": value["descripcion"],
+                            "caratula": value["caratula"],
+                            "video": value["video"],
+                            "precio": value["precio"],
+                            "genero": value["genero"],
+                            "plataforma": value["plataforma"],
+                            "datosExtra": value["datosExtra"],
+                            "calificacion": value["calificacion"],
+                            "capturas": value["capturas"]
+                        })
+            elif id == -1 and ids == "":
+                for key, value in db.getDocumento(documento).items():
+                    if value != None:
+                        videojuegos.append({
+                            "id": value["id"],
+                            "nombre": value["nombre"],
+                            "descripcion": value["descripcion"],
+                            "caratula": value["caratula"],
+                            "video": value["video"],
+                            "precio": value["precio"],
+                            "genero": value["genero"],
+                            "plataforma": value["plataforma"],
+                            "datosExtra": value["datosExtra"],
+                            "calificacion": value["calificacion"],
+                            "capturas": value["capturas"]
+                        })
+            elif ids != "":
+                for key, value in db.getDocumento(documento).items():
+                    for id in ids.split(","):
+                        if value != None and str(value["id"]) == str(id):
+                            videojuegos.append({
+                                "id": value["id"],
+                                "nombre": value["nombre"],
+                                "descripcion": value["descripcion"],
+                                "caratula": value["caratula"],
+                                "video": value["video"],
+                                "precio": value["precio"],
+                                "genero": value["genero"],
+                                "plataforma": value["plataforma"],
+                                "datosExtra": value["datosExtra"],
+                                "calificacion": value["calificacion"],
+                                "capturas": value["capturas"]
+                            })
+
+            if len(videojuegos) > 0:
+                return JsonResponse({"message": "Exitoso", f"{documento}": videojuegos})
+            else:
+                return JsonResponse(db.mensajeFallido)
+        else:
+            return JsonResponse(db.mensajePerdida)
+
+    def post(self, request):
+        if db.conexionDB and request.method == "POST":
+            jb = json.loads(request.body)
+            v = Videojuego(
+                db.getUltimateKey(documento),
+                jb["nombre"],
+                jb["descripcion"],
+                jb["caratula"],
+                jb["video"],
+                jb["precio"],
+                jb["genero"],
+                jb["plataforma"],
+                jb["datosExtra"],
+                jb["calificacion"],
+                jb["capturas"]
+            )
+
+            if v.nombre != "":
+                db.getDB().reference(documento).child(str(v.id)).set({"id": f"{v.id}", "nombre": f"{v.nombre}", "descripcion": f"{v.descripcion}", "caratula": f"{v.caratula}", "video": f"{v.video}", "precio": f"{v.precio}", "genero": f"{v.genero}", "plataforma": f"{v.plataforma}", "datosExtra": f"{v.datosExtra}", "calificacion": f"{v.calificacion}","capturas":f"{v.capturas}"})
+                return JsonResponse(db.mensajeExitoso)
+            else:
+                return JsonResponse(db.mensajeFallido)
+        else:
+            return JsonResponse(db.mensajePerdida)
+
+    def put(self, request, id):
+        if db.conexionDB:
+            jb = json.loads(request.body)
+            v = Videojuego(
+                jb["id"],
+                jb["nombre"],
+                jb["descripcion"],
+                jb["caratula"],
+                jb["video"],
+                jb["precio"],
+                jb["genero"],
+                jb["plataforma"],
+                jb["datosExtra"],
+                jb["calificacion"],
+                jb["capturas"]
+            )
+            updatekey = ""
+
+            for key, value in db.getDocumento(documento).items():
+                if value != None and str(value["id"]) == v.id and v.id == str(id):
+                    updatekey = str(key)
+                    break
+
+            if updatekey != "":
+                db.getDB().reference(documento).child(updatekey).update({"id": f"{v.id}", "nombre": f"{v.nombre}", "descripcion": f"{v.descripcion}", "caratula": f"{v.caratula}", "video": f"{v.video}", "precio": f"{v.precio}", "genero": f"{v.genero}", "plataforma": f"{v.plataforma}", "datosExtra": f"{v.datosExtra}", "calificacion": f"{v.calificacion}", "capturas":f"{v.capturas}"})
+                return JsonResponse(db.mensajeExitoso)
+            else:
+                return JsonResponse(db.mensajeFallido)    
+        else:
+            return JsonResponse(db.mensajePerdida)
+
+    def delete(self, request, id):
+        if db.conexionDB:
+            deletekey = ""
+            
+            for key, value in db.getDocumento(documento).items():
+                if value != None and str(value["id"]) == str(id):
+                    deletekey = str(key)
+                    break
+
+            if deletekey != "":
+                db.getDB().reference(documento).child(deletekey).delete()
+                return JsonResponse(db.mensajeExitoso)
+            else:
+                return JsonResponse(db.mensajeFallido)
+        else:
+            return JsonResponse(db.mensajePerdida)
--- frontend/src/componentes/Videojuego/VideojuegoForm.js
+++ frontend/src/componentes/Videojuego/VideojuegoForm.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import * as VideojuegoServer from './VideojuegoServer';
+import { MDBCarousel, MDBCarouselItem } from "mdb-react-ui-kit";
 
 const VideojuegoForm = () => {
     const history = useNavigate();
     const params = useParams();
 
-    const initialState = {id: 0, nombre: "", descripcion: "", caratula: "", video: "", precio: 0.0, genero: "", plataforma: "", datosExtra: "", calificacion: 0.0};
+    const initialState = { id: 0, nombre: "", descripcion: "", caratula: "", video: "", precio: 0.0, genero: "", plataforma: "", datosExtra: "", calificacion: 0.0, capturas: [] };
     const [videojuego, setVideojuego] = useState(initialState);
 
     const handleInputChange = (e) => {
@@ -14,55 +15,287 @@ const VideojuegoForm = () => {
     };
 
     const getVideojuego = async (idVideojuego) => {
-        try{
+        try {
             const data = await (await VideojuegoServer.getVideojuego(idVideojuego)).json();
-            const { id, nombre, descripcion, caratula, video, precio, genero, plataforma, datosExtra, calificacion } = data.Videojuegos[0];
+            const { id, nombre, descripcion, caratula, video, precio, genero, plataforma, datosExtra, calificacion, capturas } = data.Videojuegos[0];
+            console.log(capturas);
+            console.log(capturas)
+            // const x= [capturas.split(",")]
+            // console.log(x)
+            setVideojuego({ id, nombre, descripcion, caratula, video, precio, genero, plataforma, datosExtra, calificacion, capturas });
+            console.log(videojuego)
 
-            setVideojuego({ id, nombre, descripcion, caratula, video, precio, genero, plataforma, datosExtra, calificacion });
-        }catch(error){
+        } catch (error) {
             console.log(error);
         }
     };
 
     useEffect(() => {
-        if(params.id) getVideojuego(params.id);
+        if (params.id) getVideojuego(params.id);
         // eslint-disable-next-line
     }, []);
 
-    return(
+    return (
         <>
-        <div>
-            <img src={videojuego.caratula} alt="caratula"></img>
+            <div class="product-content product-wrap clearfix product-deatil" style={{
+                borderBottom: "1px solid #dfe5e9",
+                paddingBottom: "17px",
+                paddingLeft: "16px",
+                paddingTop: "16px",
+                position: "relative"
+            }}>
+                <div class="row">
+                    <div class="col-md-5 col-sm-12 col-xs-12">
+                        <div class="product-image">
+                            <div
+                                style={{
+                                    marginTop: "10%",
+                                    marginLeft: "auto",
+                                    marginRight: "auto",
+                                    width: "250ph",
+                                    height: "50%"
 
-            <div>
-                <h2>Datos Extra</h2>
-                <p>{videojuego.datosExtra}</p>
-                <button>Ver más</button>
+                                }}>
+
+                                <MDBCarousel showControls showIndicators>
+
+                                    <MDBCarouselItem
+                                        className='w-100 d-block'
+                                        itemId={1}
+                                        src={videojuego.caratula}
+                                        alt='Carátula'
+                                        height='450px'
+                                    />
+                                    <MDBCarouselItem
+
+                                    className='w-100 d-block'
+                                        itemId={3}
+                                        src={videojuego.capturas[0]}
+                                        alt='...'
+                                    
+                                    />
+
+                                    <MDBCarouselItem
+                                        className='w-100 d-block'
+                                        itemId={3}
+                                        src={videojuego.capturas[2]}
+                                        alt='...'
+                                    />
+
+
+                                </MDBCarousel>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-md-offset-1 col-sm-12 col-xs-12" style={{
+                        borderBottom: "1px solid #dfe5e9",
+                        paddingBottom: "17px",
+                        paddingLeft: "16px",
+                        paddingTop: "16px",
+                        position: "relative"
+                    }}>
+                        <h2 class="name">
+                            {videojuego.nombre}
+                        </h2>
+                        <hr />
+                        <h3 class="price-container" style={{
+                            fontSize: "24px",
+                            margin: "0",
+                            fontWeight: "300"
+                        }
+
+                        }>
+                            ${videojuego.precio}
+                            <small style={{ fontSize: "12px" }}>*includes tax</small>
+                        </h3>
+                        <div class="certified" style={{ margin: "10px" }}>
+                            <ul style={{ paddingLeft: "0" }}>
+                                <li style={
+                                    {
+                                        marginLeft: "-3",
+                                        display: "inline-block",
+                                        backgroundColor: "#f9f9f9",
+                                        border: "1px solid #ccc",
+                                        padding: "13px 19px"
+                                    }}>
+                                    <a href="javascript:void(0);">Delivery time<span style={{
+                                        display: "block",
+                                        color: "#21c2f8",
+                                        fontSize: "13px",
+                                        fontWeight: "700",
+                                        textAlign: "center"
+                                    }}>7 Working Days</span></a>
+                                </li>
+                                <li style={
+                                    {
+                                        borderRight: "none",
+                                        marginLeft: "-3",
+                                        display: "inline-block",
+                                        backgroundColor: "#f9f9f9",
+                                        border: "1px solid #ccc",
+                                        padding: "13px 19px"
+                                    }}>
+                                    <a href="javascript:void(0);" style={{
+                                        textAlign: "left",
+                                        fontSize: "12px",
+                                        color: "#6d7a83",
+                                        lineHeight: "16px",
+                                        textDecoration: "none"
+                                    }}>Certified<span style={{
+                                        display: "block",
+                                        color: "#21c2f8",
+                                        fontSize: "13px",
+                                        fontWeight: "700",
+                                        textAlign: "center"
+                                    }}>Quality Assured</span></a>
+                                </li>
+                            </ul>
+                        </div>
+                        <hr />
+                        <div class="description description-tabs" style={{
+                            padding: "30px 0 5px !important"
+                        }}>
+                            <ul id="myTab" class="nav nav-pills">
+                                <li className="active"><a href="#more-information" data-toggle="tab" class="no-margin">Product
+                                    Description </a></li>
+                                <li class=""><a href="#specifications" data-toggle="tab">Specifications</a></li>
+                                <li class=""><a href="#reviews" data-toggle="tab">Reviews</a></li>
+                            </ul>
+                            <div id="myTabContent" class="tab-content" style={{
+                                padding: "10px 0"
+                            }}>
+                                <div class="tab-pane fade active in" id="more-information">
+                                    <br />
+                                    <strong>Description Title</strong>
+                                    <p>
+                                        Integer egestas, orci id condimentum eleifend, nibh nisi pulvinar eros, vitae ornare
+                                        massa neque ut orci. Nam aliquet lectus sed odio eleifend, at iaculis dolor egestas.
+                                        Nunc elementum pellentesque augue
+                                        sodales porta. Etiam aliquet rutrum turpis, feugiat sodales ipsum consectetur nec.
+                                    </p>
+                                </div>
+                                <div class="tab-pane fade" id="specifications">
+                                    <br />
+                                    <dl class="">
+                                        <dt>Gravina</dt>
+                                        <dd>Etiam porta sem malesuada magna mollis euismod.</dd>
+                                        <dd>Donec id elit non mi porta gravida at eget metus.</dd>
+                                        <dd>Eget lacinia odio sem nec elit.</dd>
+                                        <br />
+                                        <dt>Test lists</dt>
+                                        <dd>A description list is perfect for defining terms.</dd>
+                                        <br />
+                                        <dt>Altra porta</dt>
+                                        <dd>Vestibulum id ligula porta felis euismod semper</dd>
+                                    </dl>
+                                </div>
+                                <div class="tab-pane fade" id="reviews">
+                                    <br />
+                                    <form method="post" class="well padding-bottom-10" onsubmit="return false;">
+                                        <textarea rows="2" class="form-control" placeholder="Write a review"></textarea>
+                                        <div class="margin-top-10">
+                                            <button type="submit" class="btn btn-sm btn-primary pull-right">
+                                                Submit Review
+                                            </button>
+                                            <a href="javascript:void(0);" class="btn btn-link profile-link-btn"
+                                                rel="tooltip" data-placement="bottom" title=""
+                                                data-original-title="Add Location"><i class="fa fa-location-arrow"></i></a>
+                                            <a href="javascript:void(0);" class="btn btn-link profile-link-btn"
+                                                rel="tooltip" data-placement="bottom" title=""
+                                                data-original-title="Add Voice"><i class="fa fa-microphone"></i></a>
+                                            <a href="javascript:void(0);" class="btn btn-link profile-link-btn"
+                                                rel="tooltip" data-placement="bottom" title=""
+                                                data-original-title="Add Photo"><i class="fa fa-camera"></i></a>
+                                            <a href="javascript:void(0);" class="btn btn-link profile-link-btn"
+                                                rel="tooltip" data-placement="bottom" title=""
+                                                data-original-title="Add File"><i class="fa fa-file"></i></a>
+                                        </div>
+                                    </form>
+                                    <div class="chat-body no-padding profile-message">
+                                        <ul>
+                                            <li class="message">
+                                                <img src="https://bootdey.com/img/Content/avatar/avatar1.png"
+                                                    class="online" />
+                                                <span class="message-text" style={{ width: "calc(100% - 70px)" }}>
+                                                    <a href="javascript:void(0);" class="username">
+                                                        Alisha Molly
+                                                        <span class="badge">Purchase Verified</span>
+                                                        <span class="pull-right">
+                                                            <i class="fa fa-star fa-2x text-primary"></i>
+                                                            <i class="fa fa-star fa-2x text-primary"></i>
+                                                            <i class="fa fa-star fa-2x text-primary"></i>
+                                                            <i class="fa fa-star fa-2x text-primary"></i>
+                                                            <i class="fa fa-star fa-2x text-muted"></i>
+                                                        </span>
+                                                    </a>
+                                                    Can't divide were divide fish forth fish to. Was can't form the, living
+                                                    life grass darkness very image let unto fowl isn't in blessed fill life
+                                                    yielding above all moved
+                                                </span>
+                                                <ul class="list-inline font-xs">
+                                                    <li>
+                                                        <a href="javascript:void(0);" class="text-info"><i
+                                                            class="fa fa-thumbs-up"></i> This was helpful (22)</a>
+                                                    </li>
+                                                    <li class="pull-right">
+                                                        <small class="text-muted pull-right ultra-light"> Posted 1 year ago
+                                                        </small>
+                                                    </li>
+                                                </ul>
+                                            </li>
+                                            <li class="message">
+                                                <img src="https://bootdey.com/img/Content/avatar/avatar2.png"
+                                                    class="online" />
+                                                <span class="message-text">
+                                                    <a href="javascript:void(0);" class="username">
+                                                        Aragon Zarko
+                                                        <span class="badge">Purchase Verified</span>
+                                                        <span class="pull-right">
+                                                            <i class="fa fa-star fa-2x text-primary"></i>
+                                                            <i class="fa fa-star fa-2x text-primary"></i>
+                                                            <i class="fa fa-star fa-2x text-primary"></i>
+                                                            <i class="fa fa-star fa-2x text-primary"></i>
+                                                            <i class="fa fa-star fa-2x text-primary"></i>
+                                                        </span>
+                                                    </a>
+                                                    Excellent product, love it!
+                                                </span>
+                                                <ul class="list-inline font-xs">
+                                                    <li>
+                                                        <a href="javascript:void(0);" class="text-info"><i
+                                                            class="fa fa-thumbs-up"></i> This was helpful (22)</a>
+                                                    </li>
+                                                    <li class="pull-right">
+                                                        <small class="text-muted pull-right ultra-light"> Posted 1 year ago
+                                                        </small>
+                                                    </li>
+                                                </ul>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <hr />
+                        <div class="row">
+                            <div class="col-sm-12 col-md-6 col-lg-6">
+                                <a href="javascript:void(0);" class="btn btn-success btn-lg">Add to cart (${videojuego.precio})</a>
+                            </div>
+                            <div class="col-sm-12 col-md-6 col-lg-6">
+                                <div class="btn-group pull-right">
+                                    <button class="btn btn-white btn-default"><i class="fa fa-star"></i> Add to
+                                        wishlist</button>
+                                    <button class="btn btn-white btn-default"><i class="fa fa-envelope"></i> Contact
+                                        Seller</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-
-        <div>
-            <h1>{videojuego.nombre}</h1>
-
-            <div>
-                <h2>Descripción</h2>
-                <p>{videojuego.descripcion}</p>
-            </div>
-
-            <iframe width="560" height="315" src={videojuego.video} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowfullscreen="true" allowTransparency="true"></iframe>
-        </div>
-
-        <div>
-            <h2>{videojuego.precio}</h2>
-
-            {/* Cupones */}
-
-            <button>Comprar</button>
-            <button>Reservar</button>
-            <button>Calificar</button>
-            <button>Opinar</button>
-        </div></>
-    );
+</>
+            );
 };
 
-export default VideojuegoForm;
+            export default VideojuegoForm;
