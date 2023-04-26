f50aa8a7fb7e500e4faaaac1dd97f04a1b0fdddb
BrandonVasquezL <zs20022115@estudiantes.uv.mx>
2023-04-25 19:32:10 -0600

    HomePage

--- frontend/src/componentes/Usuario/UsuarioHome.js
+++ frontend/src/componentes/Usuario/UsuarioHome.js
@@ -16,6 +16,7 @@ const UsuarioHome = () => {
             <div className={styles.contenedor}>
                 <h1 className={styles.welcome}>¡Bienvenid@ a Respawn!</h1>
                 <h3 className={styles.user}><b>{usuario.nombre}</b></h3>
+                <div className={styles.contenedor2}>
                 <h4 className={styles.registro}>Si aun no te registras</h4>
                 <a href="/signUp" className={styles.link}>
                     <span></span>
@@ -23,10 +24,18 @@ const UsuarioHome = () => {
                     <span></span> 
                     <span></span>  
                     click aquí</a>
+                    </div>
+
+                    <div className={styles.contenedor3}>
+                    <h4 className={styles.registro}>Si ya tienes cuenta</h4>
+                <a href="/login" className={styles.link}>
+                    <span></span>
+                    <span></span> 
+                    <span></span> 
+                    <span></span>  
+                    click aquí</a>
+                    </div>
             </div>
-            <audio>
-                <source src="../audio.mp3" type="audio/mpeg"/>
-            </audio>
         </>
         </html>
         
